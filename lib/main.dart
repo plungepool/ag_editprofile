b80fe6af0c2b2c11a91447a9a4e6ed5eb0b071cc
cyclopssound <cyclopssound@gmail.com>
2023-11-28 18:23:47 -0800

    Added profile photo

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: SizedBox(
-        width: 400.0,
+        width: 1000.0,
         child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              children: [IconRow(), const SizedBox(height: 50), EditProfile()],
+              children: [
+                IconRow(),
+                const SizedBox(height: 50),
+                EditProfile(),
+                const SizedBox(height: 20),
+                ProfileCircle()
+              ],
             )),
       )),
     );
@@ -56,5 +62,53 @@ class IconRow extends StatelessWidget {
       Icon(Icons.network_wifi, color: blue),
       Icon(Icons.battery_full, color: blue)
     ]);
+  }
+}
+
+class ProfileCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const CircleAvatar(
+          radius: 65,
+          backgroundColor: blue,
+          child: CircleAvatar(
+            radius: 60,
+            backgroundImage: AssetImage('default_photo.jpg'),
+          ),
+        ),
+        Positioned(
+          top: 10,
+          right: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 3,
+                  color: Colors.white,
+                ),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(
+                    50,
+                  ),
+                ),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(2, 4),
+                    color: Colors.black.withOpacity(
+                      0.3,
+                    ),
+                    blurRadius: 3,
+                  ),
+                ]),
+            child: const Padding(
+              padding: EdgeInsets.all(2.0),
+              child: Icon(Icons.edit, color: blue),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
